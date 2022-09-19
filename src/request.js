d9186684d981bb3df7ff3f8589f2c7d7407204e4
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-19 17:37:34 +0700

    fix: request api block

--- src/request.js
+++ src/request.js
@@ -1,12 +1,12 @@
 const request = {
-  // dummy: "http://api.kalbarmedia.com/api/layanan_mobil",
-  berita: "http://pallima.floodewsptk.my.id/api/berita",
-  pengurus: "http://pallima.floodewsptk.my.id/api/pengurus",
-  sekolah: "http://pallima.floodewsptk.my.id/api/sekolah",
-  ibadah: "http://pallima.floodewsptk.my.id/api/ibadah",
-  posyandu: "http://pallima.floodewsptk.my.id/api/posyandu",
-  profil: "http://pallima.floodewsptk.my.id/api/profil",
-  layanan: "http://pallima.floodewsptk.my.id/api/pelayanan",
+  // dummy: "https://api.kalbarmedia.com/api/layanan_mobil",
+  berita: "https://pallima.floodewsptk.my.id/api/berita",
+  pengurus: "https://pallima.floodewsptk.my.id/api/pengurus",
+  sekolah: "https://pallima.floodewsptk.my.id/api/sekolah",
+  ibadah: "https://pallima.floodewsptk.my.id/api/ibadah",
+  posyandu: "https://pallima.floodewsptk.my.id/api/posyandu",
+  profil: "https://pallima.floodewsptk.my.id/api/profil",
+  layanan: "https://pallima.floodewsptk.my.id/api/pelayanan",
 };
 
 export default request;
